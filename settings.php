7f9da809967b7b48a7e6edbd8375ef81acb4a5e0
Rim.A <rim.abd18@gmail.com>
2017-02-20 12:07:22 +0100

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+  <body>
+
+   <form method= "post" action="login.php">
+
+     <input name= "username"/>
+     <input name= "password"/>
+     <input type= "submit"/>
+
+  </form>
+ </body>
+</html>
