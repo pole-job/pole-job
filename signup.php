0f56bf1cac776f0da80d00eb7da5343e829565c7
Cyril Kato <cyril@flui.city>
2017-03-23 09:52:31 +0100

    Revert "Update signup.php"
    This reverts commit 12e470f3511fd2d0eb86ab82f567dfa55eb24e2a.

--- signup.php
+++ signup.php
@@ -1,36 +1,63 @@
-<?php
-session_start();
-include('bibliotheque.php');
+<!DOCTYPE html>
 
-$courriel = $_POST['courriel'];
-$motdepasse = $_POST['mot_passe'];
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>PoleJob</title>
+  </head>
 
-$requete = "SELECT courriel, motdepasse FROM adherent
-            WHERE courriel='$courriel' AND motdepasse='$motdepasse'";
-$connexion = connecter();
-$resultat = mysqli_query($connexion, $requete);
-mysqli_close($connexion);
-if (mysqli_num_rows($resultat) == 0)
-  {
-    $message = 'Mauvais courriel/mot de passe.';
-    $erreur = true;
-  }
-else
-  {
-    $message = 'Vous êtes connecté.';
-    $erreur = false;
-    $_SESSION['courriel'] = $courriel;
-  }
+  <body>
+    <header>
+      <h1>PoleJob</h1>
+    </header>
 
-///////////////////////////////////// PARTIE VISIBLE /////////////////////////////////////
+    <hr />
 
+    <nav>
+      <ul>
+        <li>S'inscrire</li>
+        <li><a href="login.php">Connexion</a></li>
+      </ul>
+    </nav>
 
-include ('entete.html');
-echo $message;
-if ($erreur)
-  {
-    echo ' <a href="Mon compte.php">Veuillez réessayer</a>';
-  }
+    <hr />
 
-?>
+    <article>
+      <h2>S'inscrire à PoleJob</h2>
 
+      <form action="signup-post.php" method="post">
+        <div>
+          <input name="full_name" type="text" placeholder="Nom complet" required>
+        </div>
+
+        <div>
+          <input name="email" type="email" placeholder="Courriel" required>
+        </div>
+
+        <div>
+          <input name="password" type="password" placeholder="Mot de passe" required>
+        </div>
+
+        <div>
+          <input name="phone" type="tel" placeholder="Téléphone" required>
+        </div>
+
+        <div>
+          <textarea name="bio" placeholder="Bio" required></textarea>
+        </div>
+
+        <div>
+          <select name="city_id">
+            <option value="1">Ville 1</option>
+            <option value="2">Ville 2</option>
+            <option value="3">Ville 3</option>
+          </select>
+        </div>
+
+        <div>
+          <input class="btn" type="submit" value="Créer mon compte">
+        </div>
+      </form>
+    </article>
+  </body>
+</html>
